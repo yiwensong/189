bac92a136f306dc8bd1b30fdbec8d27136cc8f44
yiwen song <songzgy@gmail.com>
2016-04-18 10:43:59 -0700

    working

--- hw6/nn.py
+++ hw6/nn.py
@@ -6,7 +6,7 @@ from nn_help import *
 DIGIT_PATH = 'dataset/'
 TRAIN_SAMPLES = 55000
 
-ITERATIONS = 20000
+ITERATIONS = 100000000
 
 def format_img(img):
   img = np.swapaxes(img,0,2)
@@ -30,9 +30,10 @@ def load_data_digits():
   train_img = np.array(map(lambda a: a.flatten(),train_img),dtype='double')
   test_img  = np.array(map(lambda a: a.flatten(), test_img),dtype='double')
 
-  # col_sums = train_img.sum(axis=0) + .0001
-  # train_img = train_img/col_sums[:np.newaxis]
-  # test_img  = test_img /col_sums[:np.newaxis]
+  row_sums = np.linalg.norm(train_img,axis=1) + .0001
+  train_img = (train_img.T/row_sums[:np.newaxis]).T
+  row_sums = np.linalg.norm(test_img ,axis=1) + .0001
+  test_img  = (test_img.T /row_sums[:np.newaxis]).T
 
   train_lab = np.ravel(sio.loadmat(DIGIT_PATH + 'train.mat')['train_labels'])
   train_lab = np.array(map(nn_label_format,train_lab))
@@ -62,15 +63,30 @@ def main():
   img = img[:TRAIN_SAMPLES]
   lab = lab[:TRAIN_SAMPLES]
 
-  for idx in xrange(ITERATIONS):
-    if idx % 10000 == 0:
+  rate = .00005
+  idx = 0
+  while True:
+    if idx % TRAIN_SAMPLES == 0:
+      img,lab = get_samples(TRAIN_SAMPLES,img,lab)
+
+    if idx % (TRAIN_SAMPLES/2) == 0:
       print idx
-    hidden,output = backprop(img[idx%TRAIN_SAMPLES],lab[idx%TRAIN_SAMPLES],hidden,output,learning_rate=.0005,loss=mean_sq_loss_grad)
+      lbl = [''] * (available - TRAIN_SAMPLES)
+      for idx in xrange(available - TRAIN_SAMPLES):
+        lbl[idx] = get_label(vimg[idx],hidden,output)
+      lbl = np.array(lbl)
+      vlabel_int = np.array(map(np.argmax,vlab))
+      print "Validation rate:",np.sum(lbl == vlabel_int)/float(lbl.shape[0])
+
+    # if idx % 100000 == 0:
+    #   rate = rate/2
+
+    hidden,output = backprop(img[idx%TRAIN_SAMPLES],lab[idx%TRAIN_SAMPLES],hidden,output,learning_rate=rate,loss=mean_sq_loss_grad)
+    idx = (idx + 1) % TRAIN_SAMPLES
 
   lbl = [''] * (available - TRAIN_SAMPLES)
   for idx in xrange(available - TRAIN_SAMPLES):
     lbl[idx] = get_label(vimg[idx],hidden,output)
-
   lbl = np.array(lbl)
   vlabel_int = np.array(map(np.argmax,vlab))
   print "Validation rate:",np.sum(lbl == vlabel_int)/float(lbl.shape[0])
